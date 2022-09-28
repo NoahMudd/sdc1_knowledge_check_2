a41fa2c088804f7471765f494436a726a2c7360b
Michael Ducharm <ducharmdev@gmail.com>
2022-09-27 20:09:52 -0400

    added ability to add CDs, and started on update logic

---
+++ MusicStore/CD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore
+{
+    public class CD
+    {
+        public string Title;
+
+        public string Artist;
+
+        public string Genre;
+
+        public decimal Price;
+
+        public int YearOfRelease;
+
+        public string ToString()
+        {
+            return $"{Title} by {Artist}";
+        }
+    }
+}
--- MusicStore/Program.cs
+++ MusicStore/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MusicStore;
+
+// See https://aka.ms/new-console-template for more information
 /*
  * Music Store
 
@@ -18,18 +20,20 @@
 	  - Vinyl
 	  - Tapes
 */
+
 List<CD> cds = new List<CD>();
-cds.Add(new CD() { Artist = "Justin" });
-cds.Add(new CD() { Artist = "Brad" });
-cds.Add(new CD() { Artist = "Michael" });
+cds.Add(new CD() { Title = "Booker's Dream",  Artist = "Justin" });
+cds.Add(new CD() { Title = "Straight Outta The Highlands", Artist = "Brad" });
+cds.Add(new CD() { Title = "Barely Awake", Artist = "Michael" });
 
 Console.WriteLine("Welcome to CL music store");
 
 Console.WriteLine("1. List Inventory");
 Console.WriteLine("2. Add to Inventory");
-Console.WriteLine("3. Remove from Inventory");
-Console.WriteLine("4. Sell from Inventory");
-Console.WriteLine("5. Exit");
+Console.WriteLine("3. Update existing product");
+Console.WriteLine("4. Remove from Inventory");
+Console.WriteLine("5. Sell from Inventory");
+Console.WriteLine("6. Exit");
 
 var key = Console.ReadKey().KeyChar;
 Console.WriteLine("The key you pressed was " + key);
@@ -38,24 +42,71 @@ if (key == '1')
 {
     Console.WriteLine("Inventory Contains:");
     Console.WriteLine(cds.Count);
-    foreach (var i in cds)
+    foreach (var cd in cds)
     {
-        Console.WriteLine(i.Artist);
+        Console.WriteLine(cd.ToString());
     }
-    //Console.WriteLine(myCD.Artist);
-    //Console.WriteLine(myCD2.Artist);
-    //Console.WriteLine(myCD3.Artist);
 }
-
-public class CD
+else if (key == '2')
 {
-	public string Title;
+    var newCd = new CD();
 
-    public string Artist;
+    Console.WriteLine("Creating new CD.");
+    Console.WriteLine("Title: ");
+    var title = Console.ReadLine();
 
-    public string Genre;
+    newCd.Title = title;
 
-    public decimal Price;
+    Console.WriteLine("Artist: ");
+    newCd.Artist = Console.ReadLine();
 
-    public int YearOfRelease;
+    Console.WriteLine("Genre: ");
+    newCd.Genre = Console.ReadLine();
+
+    Console.WriteLine("Price: ");
+    var price = Console.ReadLine();
+    try
+    {
+        newCd.Price = decimal.Parse(price);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Failed to include price - was not a valid decimal.");
+    }
+
+    Console.WriteLine("Year of release: ");
+    var year = Console.ReadLine();
+    try
+    {
+        newCd.YearOfRelease = int.Parse(year);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Failed to include year - was not a valid integer.");
+    }
+
+    Console.WriteLine(newCd.ToString());
+}
+else if (key == '3')
+{
+    Console.WriteLine("Choose CD to update: ");
+
+    for (int i = 0; i < cds.Count; i++)
+    {
+        var currentCD = cds[i].ToString();
+        Console.WriteLine($"{i + 1}. {currentCD}");
+    }
+
+    var selectedCDIndex = Console.ReadLine();
+
+    int index;
+
+    bool success = int.TryParse(selectedCDIndex, out index);
+
+    if (success)
+    {
+        var selectedCD = cds[index - 1];
+
+    }
+
 }
