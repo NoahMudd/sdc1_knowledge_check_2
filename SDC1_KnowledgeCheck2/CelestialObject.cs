e18b5af45385284d66420d3f09fe62d06ffc2438
Noah Mudd <noah.mudd@outlook.com>
2022-11-01 15:39:35 -0400

    initial commit

---
+++ SDC1_KnowledgeCheck2/CelestialObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDC1_KnowledgeCheck2
+{
+    public class CelestialObject
+    {
+        public string mass;
+    
+        public string composition;
+        
+        public string location;
+
+
+    }
+}
